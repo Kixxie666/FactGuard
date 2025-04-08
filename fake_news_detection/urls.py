32c30d4bac83d3de709f17e3b561ec41324f30a1
Kixxie666 <kixxie6@outlook.com>
2025-04-08 15:24:19 +0100

    Fixed API route exposure in main urls.py

--- fake_news_detection/urls.py
+++ fake_news_detection/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
+from django.contrib import admin
 from .views import classify_news
 
 urlpatterns = [
     path('classify/', classify_news, name="classify_news"),
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
 ]
