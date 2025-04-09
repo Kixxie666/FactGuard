11fe1f48198f2c64c7604bb04adc4f4d65fa9989
Kixxie666 <kixxie6@outlook.com>
2025-04-09 23:06:47 +0100

    zx

--- fake_news_detection/models.py
+++ fake_news_detection/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 
 
 class SavedWebsite(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='fnd_saved_sites')
     url = models.URLField(unique=True)
     date_added = models.DateTimeField(auto_now_add=True)
