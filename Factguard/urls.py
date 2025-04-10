b1ce350b2b6c9a4da08f63da373dc1ab1ec96497
Kixxie666 <kixxie6@outlook.com>
2025-04-10 01:22:55 +0100

    zx

--- Factguard/urls.py
+++ Factguard/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
+from django.shortcuts import render
 from django.urls import reverse
 from core.views import save_website
 
-def home_redirect(request):
-    return redirect(reverse('classify_news'))
+def home(request):
+    return render(request, "core/home.html")
 urlpatterns = [
-    path('', home_redirect),
     path('fake-news/', include('fake_news_detection.urls')),
-    path("save-website/", save_website, name="save_website"),  
+    path("save-website/", save_website, name="save_website"),
     path('', include('core.urls')),
 ]
