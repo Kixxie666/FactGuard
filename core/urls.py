80686c7e9869aafdf24a4fad1fdf369e74710b26
Kixxie666 <kixxie6@outlook.com>
2025-04-11 12:51:45 +0100

    xxx

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from django.contrib import admin
 from . import views
-from .views import post_url, vote, community_board, home, register, user_login, user_logout, save_website, profile, classify, submit_for_verification
+from .views import post_url, vote, community_board, home, register, user_login, user_logout, save_website, profile, classify, submit_for_verification, get_trending_alert
 urlpatterns = [
     path("profile/", profile, name="profile"),
     path("admin/", admin.site.urls),
@@ -17,4 +17,5 @@ urlpatterns = [
     path("submit-for-verification/", submit_for_verification, name="submit_for_verification"),
     path('api/community-posts/', views.community_posts_api, name='community-posts-api'),
     path('api/submit-vote/', views.submit_vote, name='submit-vote-api'),
+    path("get-trending-alert/", get_trending_alert, name="get_trending_alert"),
 ]
